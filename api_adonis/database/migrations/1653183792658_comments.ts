e21c3f4fa8a1a6b3d80ea59347a41ec75e53aca4
Ben Evora <65423996+benevora@users.noreply.github.com>
2022-05-21 22:00:46 -0400

    Creation of the Comment controller, and (Comment.ts, and Moment.ts) (Models) updated

--- api_adonis/database/migrations/1653183792658_comments.ts
+++ api_adonis/database/migrations/1653183792658_comments.ts
@@ -6,6 +6,10 @@ export default class Comments extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.string("username")
+      table.string("text")
+
+      table.integer("moment_id").unsigned().references("moments.id").onDelete('CASCADE')
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
