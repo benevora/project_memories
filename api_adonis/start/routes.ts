872f0812a2a78358228a4f8e76d9c1c1c36e6732
Ben Evora <65423996+benevora@users.noreply.github.com>
2022-05-21 21:05:43 -0400

    (routes.ts, package.json, and MomentsController.ts) updated
    Routes implementation:
    (1) route to create moments
    (2) route to Get all moments
    (3) route to Get a single individual moment
    (4) route to delete a moment
    (5) route to update moments

--- api_adonis/start/routes.ts
+++ api_adonis/start/routes.ts
@@ -21,7 +21,11 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
+  
   Route.get('/', async () => {
     return { hello: 'world' }
   })
+
+  Route.resource('/moments', 'MomentsController').apiOnly()
+
 }).prefix('/api')
